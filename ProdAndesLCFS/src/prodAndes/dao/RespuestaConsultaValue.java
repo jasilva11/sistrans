254ddfba7c408b9b5b7e82bac0fbed0699c30068
LauraCortes <la.cortes10@uniandes.edu.co>
2015-05-18 20:19:47 -0500

    Según presentación

--- ProdAndesLCFS/src/prodAndes/dao/RespuestaConsultaValue.java
+++ ProdAndesLCFS/src/prodAndes/dao/RespuestaConsultaValue.java
@@ -1,5 +1,13 @@
 package prodAndes.dao;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Hashtable;
+import java.util.Properties;
+
 import javax.activation.DataSource;
 import javax.jms.Connection;
 import javax.jms.ConnectionFactory;
@@ -10,57 +18,119 @@ import javax.jms.Queue;
 import javax.jms.Session;
 import javax.jms.TextMessage;
 import javax.naming.InitialContext;
+import javax.naming.NamingException;
 
 public class RespuestaConsultaValue 
 {
-	private DataSource ds;
+	private static final String ARCHIVO_CONEXION = "/conexion.properties";
 	
-	private ConnectionFactory cf;
+	/**
+	 * nombre del usuario para conectarse a la base de datos.
+	 */
+	private String usuario;
 	
+	/**
+	 * clave de conexi�n a la base de datos.
+	 */
+	private String clave;
+	
+	/**
+	 * URL al cual se debe conectar para acceder a la base de datos.
+	 */
+	private String cadenaConexion;
+	
+	public java.sql.Connection conexion;
+
 	private InitialContext context;
 
-	private Connection conn;
+	private DataSource ds1;
 
-	private Session s;
+	private ConnectionFactory cf;
 
-	private Destination d;
+	private Queue colaDefinida;
 
-	private MessageProducer mp;
-	
-	private Queue cola;
-	
+	private Connection conm;
 
 	/**
 	 * Constructor del encargado de enviar mensajes JMS.
 	 */
-	public RespuestaConsultaValue() {
-		InitialContext init;
-		try {
-			init = new InitialContext();
-			this.cf = (ConnectionFactory) init.lookup("RemoteConnectionFactory");
-			this.d = (Destination) init.lookup("queue/test");
-			this.c = (Connection) this.cf.createConnection("sistrans", "test");
-			((javax.jms.Connection) this.c).start();
-			this.s = ((javax.jms.Connection) this.c).createSession(false, Session.AUTO_ACKNOWLEDGE);
-			this.mp = this.s.createProducer(this.d);
+	public RespuestaConsultaValue() 
+	{
+		try 
+		{
+			context = new InitialContext();
+			ds1 = (DataSource) context.lookup("java:transaccionesConsulta");
+			cf =(ConnectionFactory) context.lookup("java:JmsXA");
+			colaDefinida = (Queue) context.lookup("queue/WebApp2");
+			//UserTransaction utx =(UserTransaction) context.list("/UserTransaction");
+			iniciarConexion();
+			//utx.begin();
+			Statement st = conexion.createStatement();
+			st.close();
+		
+			Session session = conm.createSession(false, Session.AUTO_ACKNOWLEDGE);
+			MessageProducer producer= session.createProducer(colaDefinida);
+			
+			TextMessage msg = session.createTextMessage();
+			//msg.setText(peticion);
+			producer.send(msg);
+			
+			//utx.xommit();
+			closeConnection();
+		} 
+		catch (SQLException e) 
+		{
+			e.printStackTrace();
+		} 
+		catch (JMSException e) 
+		{
+			e.printStackTrace();
 		} catch (Exception e) {
+			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
-
+	
 	/**
-	 * Envia un mensaje a JMS.
-	 * @param mensaje
+	 * M�todo que se encarga de crear la conexi�n con el Driver Manager
+	 * a partir de los parametros recibidos.
+	 * @param url direccion url de la base de datos a la cual se desea conectar
+	 * @param usuario nombre del usuario que se va a conectar a la base de datos
+	 * @param clave clave de acceso a la base de datos
+	 * @throws SQLException si ocurre un error generando la conexi�n con la base de datos.
 	 */
-	public void send(String mensaje) throws Exception{
-		try {
-			TextMessage tm = this.s.createTextMessage(mensaje);
-			this.mp.send(tm);
-		} catch (JMSException e) {
-			e.printStackTrace();
-		} finally {
-			c.close();
-		}
-	}
+    private void iniciarConexion() 
+    {
+    	try
+		{
+			File arch= new File("/Applications/jboss-4.2.2.GA/server/default/data/prodAndes"+ARCHIVO_CONEXION);
+			Properties prop = new Properties();
+			FileInputStream in = new FileInputStream( arch );
 
+		    prop.load( in );
+		    in.close( );
+
+			cadenaConexion = prop.getProperty("url");
+			usuario = prop.getProperty("usuario");	
+			clave = prop.getProperty("clave");	
+			final String driver = prop.getProperty("driver");
+			Class.forName(driver);
+			conexion = DriverManager.getConnection(cadenaConexion,usuario,clave);
+		
+		}
+		catch(Exception e)
+		{
+			e.printStackTrace();
+		}
+    }
+    
+    /**
+ 	 *Cierra la conexi�n activa a la base de datos. Adem�s, con=null.
+     * @param con objeto de conexi�n a la base de datos
+     * @throws SistemaCinesException Si se presentan errores de conexi�n
+     */
+    public void closeConnection() throws Exception {        
+		conexion.close();
+		conexion = null;
+    } 
 }
